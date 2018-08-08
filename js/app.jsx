706109bd8fd62ef84d0df0d82eb567a9820e903e
k.snacheva <k.snacheva@cian.ru>
2018-08-08 20:21:48 +0300

    first steps

---
+++ js/app.jsx
@@ -0,0 +1,63 @@
+
+var my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
+
+var News = React.createClass({
+    render: function() {
+        var last_news = this.props.last_news;
+        var newsTemplate = last_news.map(function (item, index) {
+            return (
+                <div key={index}>
+                    <p className="news__author">{item.author}:</p>
+                    <p className="news__text">{item.text}</p>
+                </div>
+            )
+        })
+        console.log(newsTemplate)
+        return (
+            <div className="news">
+                {newsTemplate}
+            </div>
+        );
+    }
+});
+
+var Comments = React.createClass({
+    render: function() {
+        return (
+            <div className="comments">
+                Нет новостей - комментировать нечего
+            </div>
+        );
+    }
+});
+
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Всем привет, я компонент App!
+                <News last_news={my_news}/>
+                <Comments />
+            </div>
+    );
+    }
+});
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
